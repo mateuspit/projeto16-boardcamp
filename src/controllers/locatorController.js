b53da94474b15f13afaccb1379a975396d031627
Mateus Barcelos <mateuspit@hotmail.com>
2023-05-10 08:38:16 -0300

    fix: get game add 404 error with no game found

--- src/controllers/locatorController.js
+++ src/controllers/locatorController.js
@@ -3,7 +3,8 @@ import { getGamesService } from "../services/locatorServices.js";
 export async function getGames(req, res) {
     try {
         const allGames = await getGamesService();
-        res.send(allGames.rows);
+        if (allGames.length === 0) return res.status(404).send(`Nenhum jogo encontrado no banco de dados`);
+        res.send(allGames);
     }
     catch (err) {
         console.log(err);
--- src/services/locatorServices.js
+++ src/services/locatorServices.js
@@ -1,13 +1,13 @@
 import { db } from "../database/database-connection.js";
 
 export async function getGamesService(){
-//export default function getGames() {
+    //export default function getGames() {
     //a entrada é filtrada pelos middlewares usando schemas.
     //em services ocorrem as operações, as funções
     //o resultado das funções dão a resposta nos controllers
     try {        
         const allGames = await db.query("SELECT * FROM games");
-        return allGames;
+        return allGames.rows;
     }
     catch (err) {
         console.log(err.message);
