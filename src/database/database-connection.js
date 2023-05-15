6e64e9249e38cde5cc2061649f5ccb99aeaf2645
Mateus Barcelos <mateuspit@hotmail.com>
2023-05-15 16:38:21 -0300

    feat: deplay db configs

--- src/database/database-connection.js
+++ src/database/database-connection.js
@@ -5,7 +5,9 @@ dotenv.config();
 const { Pool } = pg;
 
 const configDatabase = {
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 };
+
+if (process.env.MODE === "prod") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
